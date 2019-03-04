711bf3baf837fb62bf2711b84fcf59ffefe6eb3d
diegohyenna <dgmartins87@hotmail.com>
2019-03-04 14:17:18 -0300

    teste para dinamico

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('js/sw-util.js');
 
-const STATIC_CACHE = 'static-v1.1';
+const STATIC_CACHE = 'static-v1.2';
 const INMUTABLE_CACHE = 'inmutable-v1';
-const DYNAMIC_CACHE = 'dynamic-v1.2';
+const DYNAMIC_CACHE = 'dynamic-v1.3';
 
 const APP_SHELL = [
   // '/',
