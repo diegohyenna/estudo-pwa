49714ba9fb202f3f49e11725b8d7f2b76f464e65
diegohyenna <dgmartins87@hotmail.com>
2019-03-04 11:37:08 -0300

    atualizar service worker

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 
 let url = window.location.href;
-let swLocation = '/sw.js';
+let swLocation = '/estudo-pwa/sw.js';
 
 if(navigator.serviceWorker){
-    // if(url.includes('localhost')){
-    //     swLocation = '/sw.js';
-    // }
+    if(url.includes('localhost')){
+        swLocation = '/sw.js';
+    }
     navigator.serviceWorker.register(swLocation);
 }
 
