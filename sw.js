d011bbfe7289c6107fcd8da2870cf0956b6d909a
diegohyenna <dgmartins87@hotmail.com>
2019-03-04 14:20:37 -0300

    teste para style.css dinamico

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 importScripts('js/sw-util.js');
 
-const STATIC_CACHE = 'static-v1.2';
+const STATIC_CACHE = 'static-v1.3';
 const INMUTABLE_CACHE = 'inmutable-v1';
 const DYNAMIC_CACHE = 'dynamic-v1.3';
 
 const APP_SHELL = [
   // '/',
   'index.html',
-  // 'css/style.css',
+  'css/style.css',
   'img/favicon.ico',
   'img/avatars/hulk.jpg',
   'img/avatars/ironman.jpg',
