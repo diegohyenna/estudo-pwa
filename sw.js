b4c3efa30f813126d2d1354da90a8f5d758a5929
diegohyenna <dgmartins87@hotmail.com>
2019-03-04 12:39:19 -0300

    mods para o ios

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/sw-util.js');
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v3';
 const INMUTABLE_CACHE = 'inmutable-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 
