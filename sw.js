f6f13e8ec91389691f145f1728fcc0dda3fbb383
diegohyenna <dgmartins87@hotmail.com>
2019-03-04 14:09:50 -0300

    mods para tentar fazer style.css dinamico

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('js/sw-util.js');
 
-const STATIC_CACHE = 'static-v6';
+const STATIC_CACHE = 'static-v1.1';
 const INMUTABLE_CACHE = 'inmutable-v1';
-const DYNAMIC_CACHE = 'dynamic-v4';
+const DYNAMIC_CACHE = 'dynamic-v1.2';
 
 const APP_SHELL = [
   // '/',
@@ -65,12 +65,11 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-
   const resposta = caches.match(e.request)
     .then( res => {
       if(res){
         return res;
-      }else if(e.request.indexOf('style.css') == -1){
+      }else{
         return fetch(e.request)
           .then( newRes => {
             return atualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
